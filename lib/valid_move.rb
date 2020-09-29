0b1c23a2280a734c1b4d016b447c5f91386282f6
Rob Desz <rob.desz@gmail.com>
2020-09-29 16:37:40 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,11 @@
-def valid_move?
-end
-
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    return false
-  else
-    return true
+def valid_move? (board,index)
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      return false
+    else
+      return true
+    end
   end
 end
+
+
