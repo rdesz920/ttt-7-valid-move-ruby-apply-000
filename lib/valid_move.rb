c737a32a5bf58c9897a64e3c8ca77f2227650062
Rob Desz <rob.desz@gmail.com>
2020-09-29 16:47:53 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -16,7 +16,7 @@ def valid_move? (board,index)
   end
 
 
-  if position_taken?(board,index) == false && on_the_board(index) == true
+  if position_taken?(board,index) == false && on_the_board?(index) == true
     return true
   else
     return false
